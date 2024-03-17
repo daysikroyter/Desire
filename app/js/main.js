3087bfad53acf7331f48b7b18025d0863c23afc3
Mustang <nubchenko@gmail.com>
2024-03-17 03:02:20 +0200

    added right - side menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
 $(function () {
+  $('.nav__btn').on('click', function () {
+    $('.rightside-menu').removeClass('rightside-menu--close');
+    $('body').addClass('lock');
+  });
 
-})
+  $('.rightside-menu__close').on('click', function () {
+    $('.rightside-menu').addClass('rightside-menu--close');
+    $('body').removeClass('lock');
+  });
+
+  $(document).on('click', function (event) {
+    if ($(event.target).hasClass('lock')) {
+      $('.rightside-menu').addClass('rightside-menu--close');
+      $('body').removeClass('lock');
+    }
+  });
+});
